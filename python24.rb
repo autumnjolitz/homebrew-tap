13f9f1735ff7ce43bd24b2807b7be1ef35a60d28
Autumn Jolitz <autumn.jolitz@gmail.com>
2026-06-21 17:29:09 -0700

    style: satisfy rubocop

--- python24.rb
+++ python24.rb
@@ -1,82 +1,30 @@
-require 'formula'
+class Python24 < Formula
+  desc "This is the Homebrew formula for Python 2.4"
+  homepage "http://www.python.org/"
+  url "https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2"
+  sha256 "da104139ad3f4534482942ac02cf8f8ed9badd370ffa14f06b07c44914423e08"
 
-<<-COMMENTS
+  option "with-framework", "Do a 'Framework' build instead of a UNIX-style build."
+  option "with-universal", "Build for both 32 & 64 bit Intel."
+  option "with-static", "Build static libraries."
 
-This is the Homebrew formula for Python.
-
-Versions
---------
-This formula is currently tracking version 2.4.x.
-
-If you are looking for newer versions of Python, check out these forks:
-  2.7.x: http://github.com/mxcl/homebrew/issues/issue/1773
-  3.1.x: http://github.com/mxcl/homebrew/issues/issue/1188
-
-Options
--------
-There are a few options for customzing the build.
-  --universal: Builds combined 32-/64-bit Intel binaries.
-  --framework: Builds a "Framework" version of Python.
-  --static:    Builds static instead of shared libraries.
-
-site-packages
--------------
-The "site-packages" folder lives in the Cellar, under the "lib" folder
-for normal builds, and under the "Frameworks" folder for Framework builds.
-
-A .pth file is added to the Cellar site-packages that adds the corresponding
-HOMEBREW_PREFIX folder (/usr/local/lib/python2.4/site-packages by default)
-to sys.path. Note that this alternate folder doesn't itself support .pth files.
-
-pip / distribute
-----------------
-The pip (and distribute) formulae in Homebrew are designed only to work
-against a Homebrew-installed Python, though they provide links for
-manually installing against a custom Python.
-
-pip and distribute are installed directly into the Cellar site-packages,
-since they need to install to a place that supports .pth files.
-
-The pip & distribute formuale use the "site_packages" method defined here
-to get the appropriate site-packages path.
-
-COMMENTS
-
-
-# Was a Framework build requested?
-def build_framework?; ARGV.include? '--framework'; end
-
-# Are we installed or installing as a Framework?
-def as_framework?
-  (self.installed? and File.exists? prefix+"Frameworks/Python.framework") or build_framework?
-end
-
-class Python24 <Formula
-  url 'https://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2'
-  homepage 'http://www.python.org/'
-  sha256 'da104139ad3f4534482942ac02cf8f8ed9badd370ffa14f06b07c44914423e08'
-
-  depends_on 'sqlite' => :optional    # Prefer over OS X's older version
-  depends_on 'readline' => :optional  # Prefer over OS X's libedit
-  depends_on 'gdbm' => :optional
-
-  option "framework", "Do a 'Framework' build instead of a UNIX-style build."
-  option "universal", "Build for both 32 & 64 bit Intel."
-  option "static", "Build static libraries."
-
-  patch :p1, :DATA
+  depends_on "gdbm" => :optional
+  depends_on "readline" => :optional  # Prefer over OS X's libedit
+  depends_on "sqlite" => :optional    # Prefer over OS X's older version
 
   # Skip binaries so modules will load; skip lib because it is mostly Python files
-  skip_clean ['bin', 'lib']
+  skip_clean ["bin", "lib"]
+
+  patch :p1, :DATA
 
   def site_packages
     # The Cellar location of site-packages
     if build.with? "framework"
       # If we're installed or installing as a Framework, then use that location.
-      return frameworks / "Python.framework"/"Versions"/"2.4"/"lib"/"python2.4"/"site-packages"
+      frameworks / "Python.framework"/"Versions"/"2.4"/"lib"/"python2.4"/"site-packages"
     else
       # Otherwise, use just the lib path.
-      return lib / "python2.4"/"site-packages"
+      lib / "python2.4"/"site-packages"
     end
   end
 
@@ -86,7 +34,7 @@ class Python24 <Formula
   end
 
   def validate_options
-    if build.with? "framework" and build.with? "static"
+    if (build.with? "framework") && (build.with? "static")
       onoe "Cannot specify both framework and static."
       exit 99
     end
@@ -97,20 +45,21 @@ class Python24 <Formula
 
     args = ["--prefix=#{prefix}", "--disable-toolbox-glue"]
 
-    if build.with? 'universal'
-      args << "--enable-universalsdk=/" << "--with-universal-archs=intel"
+    if build.with? "universal"
+      args << "--enable-universalsdk=/"
+      args << "--with-universal-archs=intel"
     end
 
     if build.with? "framework"
-      args << "--enable-framework=#{prefix}/Frameworks"
-    else
-      args << "--enable-shared" unless build.with? 'static'
+      args << "--enable-framework=#{frameworks}"
+    elsif build.without? "static"
+      args << "--enable-shared"
     end
 
     system "./configure", *args
     system "make"
     ENV.deparallelize # Some kinds of installs must be serialized.
-    system "make install"
+    system "make", "install"
 
     # Add the Homebrew prefix path to site-packages via a .pth
     prefix_site_packages.mkpath
@@ -151,7 +100,11 @@ class Python24 <Formula
 
     s = site_caveats+general_caveats
     s = framework_caveats + s if build.with? "framework"
-    return s
+    s
+  end
+
+  test do
+    system "${bin}/python2.4", "--help"
   end
 end
 __END__
