f45e12fd70b8856df7c1b773ba136cfbd9813db8
Autumn Jolitz <autumn.jolitz@gmail.com>
2026-07-13 18:29:24 -0700

    style(zope211): fix

--- zope@211.rb
+++ zope@211.rb
@@ -37,7 +37,7 @@ class ZopeAT211 < Formula
 
     mkdir_p libexec
 
-    Dir.entries(bin.to_s).reject {|f| File.directory?(f)}.each do |file|
+    Dir.entries(bin.to_s).reject { |f| File.directory?(f) }.each do |file|
       mv bin / file, libexec / file
       (prefix, suffix) = file.split(".")
       prefixed_file = "#{prefix}-2.11.#{suffix}"
