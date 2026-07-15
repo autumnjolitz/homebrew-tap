d32e66ccd8367dcc920f2c338e4c3ad21e02be55
Autumn Jolitz <autumn.jolitz@gmail.com>
2026-07-14 21:14:07 -0700

    style(python27): fix rubocop complaint

--- python@27.rb
+++ python@27.rb
@@ -74,7 +74,7 @@ class PythonAT27 < Formula
     ldflags  = []
     cppflags = []
 
-    if OS.mac? && ! MacOS.sdk_path.nil?
+    if OS.mac? && !MacOS.sdk_path.nil?
       # Help Python's build system (setuptools/pip) to build things on SDK-based systems
       # The setup.py looks at "-isysroot" to get the sysroot (and not at --sysroot)
       cflags  << "-isysroot #{MacOS.sdk_path}" << "-I#{MacOS.sdk_path}/usr/include"
