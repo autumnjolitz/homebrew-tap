fb300ee1edea312b05b2a3e7fade0959781eb045
Autumn Jolitz <autumn.jolitz@gmail.com>
2026-07-15 22:14:40 -0700

    test(python27): correct pip name

--- python@27.rb
+++ python@27.rb
@@ -366,7 +366,7 @@ class PythonAT27 < Formula
     # system "#{bin}/python2.7", "-c", "import Tkinter; root = Tkinter.Tk()"
     system "#{bin}/python2.7", "-c", "import dbm; print(dbm.library)"
     system "#{bin}/python2.7", "-c", "import zlib"
-    system bin/"pip-2.7", "list", "--format=columns"
+    system bin/"pip2.7", "list", "--format=columns"
   end
 end
 __END__
